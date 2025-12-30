53a50bac8cb68cd4feb72c6ddcc3e258b0421301
姚泰然 <yaotairan@gmail.com>
2025-12-30 17:08:40 +0800

    feat: auth

---
+++ api/src/main/java/com/hbcy/authcenter/api/modules/core/auth/controller/UserAuthController.java
@@ -0,0 +1,8 @@
+package com.hbcy.authcenter.api.modules.core.auth.controller;
+
+/**
+ * @author 姚泰然
+ * @date 2025-12-30 17:07
+ */
+public class UserAuthController {
+}
---
+++ api/src/main/java/com/hbcy/authcenter/api/modules/core/auth/service/UserAuthService.java
@@ -0,0 +1,11 @@
+package com.hbcy.authcenter.api.modules.core.auth.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * @author 姚泰然
+ * @date 2025-12-30 17:08
+ */
+@Service
+public class UserAuthService {
+}
