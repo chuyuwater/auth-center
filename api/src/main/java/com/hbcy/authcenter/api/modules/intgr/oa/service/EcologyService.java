8996bbd78f2891a369dfdafa898277b64e5c5044
姚泰然 <yaotairan@gmail.com>
2026-01-20 14:46:46 +0800

    fix: use double check

--- api/src/main/java/com/hbcy/authcenter/api/modules/intgr/oa/service/EcologyService.java
+++ api/src/main/java/com/hbcy/authcenter/api/modules/intgr/oa/service/EcologyService.java
@@ -28,8 +28,8 @@ import java.util.concurrent.TimeUnit;
  */
 @Service
 public class EcologyService {
-    public static final String LOCK_KEY = "portal:authCenter:oa:token:lock";
-    public static final String TOKEN_KEY = "portal:authCenter:oa:token";
+    public static final String LOCK_KEY = "portal:oa:token:lock";
+    public static final String TOKEN_KEY = "portal:oa:token";
     @Resource
     private StringRedisTemplate stringRedisTemplate;
     @Resource
@@ -72,33 +72,42 @@ public class EcologyService {
         return getOaAccessHeaders(user.getSrcId());
     }
 
+    private String tryFetchToken() {
+        String token = stringRedisTemplate.opsForValue().get(TOKEN_KEY);
+        if (StringUtils.isNotBlank(token)) {
+            return token;
+        }
+        if (redissonDistributedLock.tryLock(LOCK_KEY, TimeUnit.SECONDS, 10, 30)) {
+            //已经被更新了，直接返回
+            token = stringRedisTemplate.opsForValue().get(TOKEN_KEY);
+            if (StringUtils.isNotBlank(token)) {
+                return token;
+            }
+            try {
+                String encryptSecret = rsa.encryptBase64(secret, CharsetUtil.CHARSET_UTF_8, KeyType.PublicKey);
+                String respStr = ecologyClient.applyToken(appId, encryptSecret, "3600");
+                OaApplyTokenResp resp = JsonUtils.readValue(respStr, OaApplyTokenResp.class);
+                if (resp == null) {
+                    throw new ServerError("服务通信错误，请重试");
+                }
+                if (resp.getCode() == 0) {
+                    token = resp.getToken();
+                    stringRedisTemplate.opsForValue().set(TOKEN_KEY, token, 3600, TimeUnit.SECONDS);
+                }
+            } finally {
+                redissonDistributedLock.unlock(LOCK_KEY);
+            }
+        }
+        return stringRedisTemplate.opsForValue().get(TOKEN_KEY);
+    }
+
     /**
      * 获取OA访问header，相当于服务端替用户完成了登录
      * @param userId oa中的用户id，不是手机号
      * @return 跳转链接
      */
     public OaAccessHeaders getOaAccessHeaders(String userId) {
-        String token = stringRedisTemplate.opsForValue().get(TOKEN_KEY);
-        if (StringUtils.isBlank(token)) {
-            if (redissonDistributedLock.tryLock(LOCK_KEY, TimeUnit.SECONDS, 30, 60)) {
-                try {
-                    String encryptSecret = rsa.encryptBase64(secret, CharsetUtil.CHARSET_UTF_8, KeyType.PublicKey);
-                    String respStr = ecologyClient.applyToken(appId, encryptSecret, "3600");
-                    OaApplyTokenResp resp = JsonUtils.readValue(respStr, OaApplyTokenResp.class);
-                    if (resp == null) {
-                        throw new ServerError("服务通信错误，请重试");
-                    }
-                    if (resp.getCode() == 0) {
-                        token = resp.getToken();
-                        stringRedisTemplate.opsForValue().set(TOKEN_KEY, token, 3600, TimeUnit.SECONDS);
-                    }
-                } finally {
-                    redissonDistributedLock.unlock(LOCK_KEY);
-                }
-            } else {
-                token = stringRedisTemplate.opsForValue().get(TOKEN_KEY);
-            }
-        }
+        String token = tryFetchToken();
         if (StringUtils.isBlank(token)) {
             throw new ServerError("服务通信错误，请重试");
         }
