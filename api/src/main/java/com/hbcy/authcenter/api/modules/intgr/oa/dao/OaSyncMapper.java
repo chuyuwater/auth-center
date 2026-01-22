e0e96728314343d28eac813f95fcf86097fd9c50
姚泰然 <yaotairan@gmail.com>
2026-01-22 10:18:56 +0800

    fix: 缩小依赖

---
+++ api/src/main/java/com/hbcy/authcenter/api/modules/intgr/oa/dao/OaSyncMapper.java
@@ -0,0 +1,7 @@
+package com.hbcy.authcenter.api.modules.intgr.oa.dao;
+
+/**
+ * @author 姚泰然
+ * @date 2026-01-21 14:13
+ */public class OaSyncMapper {
+}
---
+++ api/src/main/java/com/hbcy/authcenter/api/modules/intgr/oa/dto/OaPersonListDTO.java
@@ -0,0 +1,7 @@
+package com.hbcy.authcenter.api.modules.intgr.oa.dto;
+
+/**
+ * @author 姚泰然
+ * @date 2026-01-21 17:52
+ */public class OaPersonListDTO {
+}
