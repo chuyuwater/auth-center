c108b581df16d38b4a00bdfebd5535efb8785d52
姚泰然 <yaotairan@gmail.com>
2026-02-04 08:37:37 +0800

    feat: add api to receive oa msg for debug

--- api/src/main/java/com/hbcy/authcenter/api/modules/intgr/oa/controller/OaAccessController.java
+++ api/src/main/java/com/hbcy/authcenter/api/modules/intgr/oa/controller/OaAccessController.java
@@ -1,14 +1,14 @@
 package com.hbcy.authcenter.api.modules.intgr.oa.controller;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import com.hbcy.authcenter.api.modules.intgr.oa.dto.OaAccessDataDTO;
 import com.hbcy.authcenter.api.modules.intgr.oa.service.EcologyService;
+import com.hbcy.common.base.json.JsonUtils;
 import jakarta.annotation.Resource;
 import jakarta.validation.constraints.NotBlank;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * OA单点登录
@@ -19,6 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping
 @Validated
+@Slf4j
 public class OaAccessController {
     @Resource
     private EcologyService ecologyService;
@@ -36,18 +37,28 @@ public class OaAccessController {
      * 将oa的待办id转为跳转链接
      * @return 使用ajax请求返回值中的uri，并将其他字段作为header，根据返回值跳转（iframe或打开新页面）
      */
-    @GetMapping("api/portal/v1/oa/workflow/")
+    @GetMapping("api/portal/v1/oa/workflow")
     public OaAccessDataDTO getTodoPage(@NotBlank(message = "requestId不能为空") String requestId) {
         return ecologyService.accessWorkflow(requestId);
     }
 
     /**
      * 同步组织信息，内部调用
+     * @ignore
      * @param oaOrgId 楚禹公司oa orgId
      * @param tenantId 楚禹公司租户id
      */
     @PostMapping("inner/portal/oa/sync")
     public void syncOrg(String oaOrgId, String tenantId) {
         ecologyService.sync(oaOrgId, tenantId);
+    }
+
+    /**
+     * 接收oa消息推送
+     * 该功能应使用ak+白名单功能，开放给OA系统调用
+     */
+    @PostMapping("api/portal/v1/oa/msg")
+    public void notifyMsg(@RequestBody JsonNode body) {
+        log.info("==========receive oa msg push:{}", JsonUtils.toJsonStr(body));
     }
 }
