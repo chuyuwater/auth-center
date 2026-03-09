81f9fb79118aaccae1a9c675e1b1ad2c9ef4f7ca
姚泰然 <yaotairan@gmail.com>
2026-03-09 11:26:44 +0800

    fix(dict): miss dictType

--- api/src/main/java/com/hbcy/authcenter/api/modules/sys/dict/vo/DictCreateVO.java
+++ api/src/main/java/com/hbcy/authcenter/api/modules/sys/dict/vo/DictCreateVO.java
@@ -3,6 +3,7 @@ package com.hbcy.authcenter.api.modules.sys.dict.vo;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import org.hibernate.validator.constraints.Range;
 
 /**
  * 字典更新/创建VO
@@ -18,4 +19,11 @@ public class DictCreateVO extends DictUpdateVO {
      */
     @NotBlank(message = "parentId不能为空")
     private String parentId;
+
+
+    /**
+     * 字典类型，0-列表，1-树状
+     */
+    @Range(min = 0, max = 1, message = "dictType范围0-1")
+    private int dictType;
 }
